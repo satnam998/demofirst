54bf62dab20bec48b34f9801a3b6b96aafc49a20
satnam998 <satnam.singh@lngconsultancy.co.in>
2021-01-22 16:39:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demofirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
